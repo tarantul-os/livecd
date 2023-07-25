23edd4cdd203bf122406def71737f0d980f091a5
tmasikt@tarantulos.com <tmasikt@tarantulos.com>
2023-07-25 10:59:02 +0300

    added calamares

---
+++ airootfs/root/.#customize_airootfs.sh
@@ -0,0 +1,1 @@
+root@awdawdawd.647
--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,5 +1,21 @@
 echo """
 sxhkd &
 calamares &
+picom -b --animations --animation-window-mass 0.3 --animation-for-open-window zoom
+feh --bg-scale /root/.confug/wall.png
+flameshot &
+~/.config/polybar/launch.sh &
+
 exec bspwm
-""" > .xinitrc
+
+bspc monitor -d 1 2 3
+bspc config border_width         0
+bspc config window_gap          50
+
+bspc config split_ratio          0.52
+bspc config borderless_monocle   true
+bspc config gapless_monocle      true
+bspc config focus_follows_pointer true
+
+""" > /root/.xinitrc
+rm /etc/X11/xinit/xinitrc
